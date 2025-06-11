314d736386496ceb0c17ac0440b81ee2fff20ad7
Raffi <raffidarmawan0411@gmail.com>
2025-06-11 12:33:46 +0700

    Added code

---
+++ 2-foundations/activity/Activity/app/src/main/java/android/app/activity/MainActivity.kt
@@ -0,0 +1,49 @@
+package android.app.activity
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Toast.makeText(this@MainActivity, "onCreate called", Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this@MainActivity, "onStart called", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this@MainActivity, "onResume called", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this@MainActivity, "onPause called", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this@MainActivity, "onStop called", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this@MainActivity, "onDestroy called", Toast.LENGTH_SHORT).show()
+    }
+}
