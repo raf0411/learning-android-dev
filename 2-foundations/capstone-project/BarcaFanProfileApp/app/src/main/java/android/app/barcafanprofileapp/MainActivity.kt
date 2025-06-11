c6edface67cead8c9ebe4e2d2553063a5022db7c
Raffi <raffidarmawan0411@gmail.com>
2025-06-11 22:58:35 +0700

    changed background color

--- 2-foundations/capstone-project/BarcaFanProfileApp/app/src/main/java/android/app/barcafanprofileapp/MainActivity.kt
+++ 2-foundations/capstone-project/BarcaFanProfileApp/app/src/main/java/android/app/barcafanprofileapp/MainActivity.kt
@@ -11,11 +11,6 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    private val INCH_VALUE = 2.54
-
-    private lateinit var inchesInput: EditText
-    private lateinit var convertButton: Button
-    private lateinit var textViewCentimetres: TextView
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,20 +18,7 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        inchesInput = findViewById(R.id.editTextInches)
-        convertButton = findViewById(R.id.buttonConvert)
-        textViewCentimetres = findViewById(R.id.textViewConvert)
 
-        // 1 inch -> 2.54 cm
-
-        convertButton.setOnClickListener {
-            if (!inchesInput.text.toString().isEmpty()) {
-                val result = inchesInput.text.toString().toDouble() * INCH_VALUE
-                textViewCentimetres.text = "Result: $result"
-            } else {
-                textViewCentimetres.text = getString(R.string.text)
-            }
-        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
