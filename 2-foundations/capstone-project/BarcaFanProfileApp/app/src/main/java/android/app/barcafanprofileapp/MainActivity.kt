89b3e57246f9ae6de5376e7c8b93737d7f2d5158
Raffi <raffidarmawan0411@gmail.com>
2025-06-18 20:50:49 +0700

    added code

--- 2-foundations/capstone-project/BarcaFanProfileApp/app/src/main/java/android/app/barcafanprofileapp/MainActivity.kt
+++ 2-foundations/capstone-project/BarcaFanProfileApp/app/src/main/java/android/app/barcafanprofileapp/MainActivity.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
